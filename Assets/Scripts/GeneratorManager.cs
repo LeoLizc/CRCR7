5a4fba2e28a827b1e58cd792f9f001ab46002f44
breynner1 <breynner2015@gmail.com>
2022-09-29 14:09:46 -0500

    generador

--- Assets/Scripts/GeneratorManager.cs
+++ Assets/Scripts/GeneratorManager.cs
@@ -8,13 +8,13 @@ public class GeneratorManager : MonoBehaviour
     [SerializeField] private GameObject streetGenerator;
 
     [SerializeField] private GameObject street;
-    [SerializeField] float velocity;
+    [SerializeField] float velocity, velocity2;
     [SerializeField] float streetVelocity;
     public string pattern;
     private int step;
-    private float nextActionTime = 0.0f;
-    [SerializeField] private float period;
-    private GameObject[] Cars;
+    private float nextActionTime = 0.0f, nextActionTime2 = 0.0f;
+    [SerializeField] private float period, perio2;
+    private GameObject[] Cars, poderes;
 
     // Start is called before the first frame update
     void Awake()
@@ -33,6 +33,15 @@ public class GeneratorManager : MonoBehaviour
             //Debug.Log("generar");
             generatePlatform();
             generateStreet();
+            LoadSuperObjects();
+            generatePower();
+        }
+        if (time > nextActionTime2)
+        {
+            nextActionTime2 = time + perio2;
+            //Debug.Log("generar");
+            LoadSuperObjects();
+            generatePower();
         }
     }
 
@@ -46,8 +55,26 @@ public class GeneratorManager : MonoBehaviour
         }
     }
 
+    void LoadSuperObjects()
+    {
+        object[] loadedCars = Resources.LoadAll("Poderes_car", typeof(GameObject));
+        poderes = new GameObject[loadedCars.Length];
+        for (int x = 0; x < loadedCars.Length; x++)
+        {
+            poderes[x] = (GameObject)loadedCars[x];
+        }
+    }
 
-    private void generatePlatform()
+
+    private void generatePower()
+    {
+
+        int Num =Random.Range(0, poderes.Length);
+        generators[(int)(Random.Range(0, 6))].GetComponent<PlatformGenerator>().generatePlatform(poderes[Num], velocity2);
+
+    }
+
+        private void generatePlatform()
     {
         string[] steps = pattern.Split(';');
         string next = steps[step%steps.Length];
