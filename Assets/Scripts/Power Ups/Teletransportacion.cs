68609430fac871e27122f437feb9070aa3a74f11
breynner1 <breynner2015@gmail.com>
2022-10-07 00:28:22 -0500

    final

--- Assets/Scripts/Power Ups/Teletransportacion.cs
+++ Assets/Scripts/Power Ups/Teletransportacion.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "StateEffects/Buffs/Teletransportacion")]
 public class Teletransportacion : PowerUp
 {
-    public float cantidad, distancia;
+    public float distancia;
     movimiento a;
     public LayerMask mask;
     public override void apply(GameObject target)
@@ -36,43 +36,6 @@ public class Teletransportacion : PowerUp
         float distance = Physics2D.Raycast(new Vector2(pos.x, pos.y), direction, 20, mask).distance;
         direction = direction * Mathf.Min(distance - radio, distancia);
         target.transform.position = new Vector3(pos.x + direction.x, pos.y+direction.y, pos.z);
-
-        /*else if (Input.GetKey("w"))
-        {
-            if (target.transform.position.y + distancia < 7.25 - 0.5)
-            {
-                target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + distancia, target.transform.position.z);
-            }
-            else
-            {
-                target.transform.position = new Vector3(target.transform.position.x, 7.25f - 0.5f, target.transform.position.z);
-            }
-
-        }
-        else if (Input.GetKey("s"))
-        {
-            if (target.transform.position.x - distancia > 0.249999 + 0.5)
-            {
-                target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y - distancia, target.transform.position.z);
-            }
-            else
-            {
-                target.transform.position = new Vector3(target.transform.position.x, 0.249999f + 0.5f, target.transform.position.z);
-            }
-
-        }
-        else
-        {
-            if (target.transform.position.x + distancia < -0.3362226 - 0.5)
-            {
-                target.transform.position = new Vector3(target.transform.position.x + distancia, target.transform.position.y, target.transform.position.z);
-            }
-            else
-            {
-                target.transform.position = new Vector3(-0.3362226f - 0.5f, target.transform.position.y, target.transform.position.z);
-            }
-
-        }*/
 
     }
     public override void deapply(GameObject target)
